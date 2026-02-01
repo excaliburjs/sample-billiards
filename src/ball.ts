01d8561debdcbff0c2d389dfa7bbccc1c6006623
Erik Onarheim <erik.onarheim@gmail.com>
2026-02-01 17:02:14 -0600

    the rolling looks good

--- src/ball.ts
+++ src/ball.ts
@@ -1,4 +1,4 @@
-import { Actor, Circle, Text, CollisionType, Color, GraphicsGroup, Rectangle, vec, Vector, Font, TextAlign } from "excalibur";
+import { Actor, Circle, Text, CollisionType, Color, GraphicsGroup, Rectangle, vec, Vector, Font, TextAlign, Engine, Label, BaseAlign, Material, clamp, Canvas, ImageSource, DegreeOfFreedom } from "excalibur";
 import { Config } from "./config";
 
 const ballColors = [
@@ -23,104 +23,126 @@ const ballColors = [
 
 
 export class Ball extends Actor {
-  constructor(pos: Vector, number: number) {
+  billardsMat!: Material;
+  textImage: ImageSource;
+  originalPos: Vector;
+  constructor(pos: Vector, public number: number) {
     super({
       pos,
       radius: Config.BallRadius,
       color: ballColors[number - 1],
       collisionType: CollisionType.Active
     });
+    this.body.bounciness = Config.Bounciness;
+    this.body.limitDegreeOfFreedom.push(DegreeOfFreedom.Rotation);
+    this.graphics.color = ballColors[number - 1];
+
+    this.originalPos = pos.clone();
 
     const font = new Font({
       family: 'sans-serif',
       size: 16,
       bold: true,
-      textAlign: TextAlign.Center
+      textAlign: TextAlign.Center,
+      baseAlign: BaseAlign.Middle
     });
 
-    if (number > 8) { // striped
-      this.graphics.use(
-        new GraphicsGroup({
-          members: [
-            {
-              // white
-              offset: vec(-.5, -.5),
-              graphic: new Circle({
-                color: Color.White,
-                strokeColor: ballColors[number - 1],
-                lineWidth: 2,
-                radius: Config.BallRadius
-              })
-            },
-            {
-              // stripe 1
-              offset: vec(4, 10),
-              graphic: new Rectangle({
-                color: ballColors[number - 1],
-                width: Config.BallRadius * 2 - 2,
-                height: 20
-              })
-            },
-            {
-              // stripe 2
-              offset: vec(2, 15),
-              graphic: new Rectangle({
-                color: ballColors[number - 1],
-                width: Config.BallRadius * 2 + 2,
-                height: 10
-              })
-            },
-            {
-              // number circle
-              offset: vec(Config.BallRadius - Config.BallRadius / 2 - 2, Config.BallRadius - Config.BallRadius / 2 - 2),
-              graphic: new Circle({
-                color: Color.White,
-                strokeColor: ballColors[number - 1],
-                lineWidth: 2,
-                radius: Config.BallRadius / 2 + 2
-              })
-            },
-            {
-              // number text
-              useBounds: false,
-              offset: vec(Config.BallRadius + 2, Config.BallRadius - 4.5),
-              graphic: new Text({
-                text: number.toString(),
-                color: Color.Black,
-                font
-              })
+    const canvas = new Canvas({
+      width: Config.BallRadius * 2,
+      height: Config.BallRadius * 2,
+      draw: (ctx) => {
+        ctx.save();
+        ctx.textAlign = font.textAlign;
+        ctx.textBaseline = font.baseAlign;
+        ctx.font = '24px sans-serif';//font.fontString;
+        ctx.fillStyle = 'black';
+        // ctx.fillText("TEST", 0, 0);
+        ctx.fillRect(5, 5, 5, 5);
+        ctx.restore();
+      }
+    });
+    canvas.flagDirty();
+    this.textImage = ImageSource.fromHtmlCanvasElement(canvas._bitmap);
 
-            }
-          ]
-        }));
-    } else {
+  }
 
-      this.graphics.use(
-        new GraphicsGroup({
-          members: [
-            {
-              offset: vec(0, 0),
-              graphic: new Circle({
-                color: ballColors[number - 1],
-                strokeColor: Color.White,
-                lineWidth: 2,
-                radius: Config.BallRadius
-              })
-            },
+   onInitialize(engine: Engine): void {
 
-            {
-              useBounds: false,
-              offset: vec(Config.BallRadius + 2, Config.BallRadius - 4.5),
-              graphic: new Text({
-                text: number.toString(),
-                color: Color.White,
-                font
-              })
-            }
-          ]
-        }))
-    }
+    const glsl = (x: any) => x[0];
+    console.log(this.graphics.color);
+    this.billardsMat = this.graphics.material = engine.graphicsContext.createMaterial({
+      name: 'billiards',
+      fragmentSource:
+        glsl`#version 300 es
+        precision mediump float;
 
-    this.body.bounciness = Config.Bounciness;
+        uniform vec4 color;
+        uniform float number;
+        uniform sampler2D u_graphic;
+        uniform sampler2D text;
+        uniform vec2 roll;
+        uniform float u_time_ms;
+
+        in vec2 v_uv;
+
+        out vec4 fragColor;
+
+        void main(){
+          vec2 uv = v_uv;
+          vec2 scroll; 
+          scroll.x = uv.x - roll.x;
+          scroll.y = uv.y - roll.y;
+
+          uv.x = mod(scroll.x, 1.);
+          uv.y = mod(scroll.y, 1.);
+
+          float dist = 1.0 - length(uv * 2.0 - 1.0);
+          float fade = fwidth(dot(v_uv, v_uv));
+
+          fragColor = color;
+
+          // "Stripe" pattern for number 9 to 15
+          if (number > 8.) {
+            fragColor.rgb = mix(vec3(1.), fragColor.rgb, smoothstep(0.15 - fade / 2., 0.15 + fade / 2., uv.y));
+            fragColor.rgb = mix(fragColor.rgb, vec3(1.), smoothstep(0.85 - fade / 2., 0.85 + fade / 2., uv.y));
+          }
+
+          // Circle for Text
+          fragColor.rgb = mix(fragColor.rgb, vec3(1.), smoothstep(.45 - fade / 2., .45 + fade / 2., dist));
+
+          // Text
+          // vec4 textcolor = texture(text, uv);
+          // fragColor.rgb = textcolor.rgb;// mix(fragColor.rgb, textcolor.rgb, textcolor.a);
+
+          // Outer edge
+          fragColor.a = texture(u_graphic, v_uv).a;
+
+
+          // premult alpha
+          fragColor.rgb *= fragColor.a;
+        }
+      `,
+      uniforms: {
+        color: this.graphics.color!,
+        number: this.number,
+        roll: vec(0, 0)
+      }
+    });
+
+    // this.textImage.ready.then(() => {
+    //   // this.billardsMat.addImageSource('text', this.textImage);
+    //   document.body.appendChild(this.textImage.image);
+    //   this.billardsMat.update(shader => {
+    //     shader.addImageSource('text', this.textImage);
+    //   });
+    // });
+
+  }
+
+  _rolling: Vector = vec(0, 0);
+  onPreUpdate(engine: Engine, elapsed: number): void {
+    this._rolling = this.pos.sub(this.originalPos).scale(1 / (2 * Config.BallRadius));
+    this.billardsMat.uniforms.roll = this._rolling;
+
   }
 }
